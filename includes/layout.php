48969fdfce1201055cff547f77b59f0f446e8cb3
israaboy <israel.t.ribeiro@gmail.com>
2026-05-21 14:40:28 -0300

    Correção path do css e js no layout

--- includes/layout.php
+++ includes/layout.php
@@ -9,11 +9,11 @@ function layout_head(string $titulo = 'Formulários'): void { ?>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Roboto+Mono:wght@400;500&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/app.css">
+<link rel="stylesheet" href="/generate_word/assets/css/app.css">
 </head>
 <body>
 <nav class="navbar">
-  <a class="navbar-brand" href="<?= BASE_URL ?>/">
+  <a class="navbar-brand" href="/generate_word/">
     <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/><polyline points="10 9 9 9 8 9"/></svg>
     Formulários
   </a>
@@ -29,7 +29,7 @@ function layout_head(string $titulo = 'Formulários'): void { ?>
 
 function layout_foot(): void { ?>
 </main>
-<script src="<?= BASE_URL ?>/assets/js/app.js"></script>
+<script src="/generate_word/assets/js/app.js"></script>
 </body>
 </html>
 <?php }
